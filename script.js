dbfd526ff97d5cc9b2c837bead89ae4e9793b823
Saba-fs <saba.fs7k@gmail.com>
2024-09-25 18:40:36 +0530

    bmi-calc

---
+++ script.js
@@ -0,0 +1,44 @@
+const bmitext=document.getElementById('bmi')
+const desctext=document.getElementById('desc')
+const form=document.querySelector('form')
+
+form.addEventListener('submit',onFormSubmit)
+
+function onFormSubmit(e)
+{
+    e.preventDefault();
+
+    const weight=parseFloat(form.wt.value)
+    const height=parseFloat(form.ht.value)
+
+    if(isNaN(weight) || isNaN(height) || weight<=0 || height<=0){
+        alert('Please enter a valid weight and height')
+        return;
+    }
+
+    const heightInMeter= height/100
+    const bmi= weight/Math.pow(heightInMeter,2)
+    const desc=interpretBMI(bmi)
+
+    bmitext.textContent=bmi.toFixed(2)
+    bmitext.className=desc
+    desctext.innerHTML=`You are <strong>${desc}</strong>`
+}
+
+function interpretBMI(bmi)
+{
+    if(bmi<18.5)
+    {
+        return"underweight"
+    }
+    else if(bmi<25){
+         return"healthy"
+    }
+    else if(bmi<30)
+    {
+        return "overweight"
+    }
+    else{
+        return "obesity"
+    }
+}
